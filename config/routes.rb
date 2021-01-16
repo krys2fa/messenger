0efc7bf6b673888f42cff83c9b54a9172d978595
Christopher Amanor <kriskit7@gmail.com>
2021-01-16 20:33:05 +0000

    Implement devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
+# Rails.application.routes.draw do
+#   devise_for :users
+#   root to: "users#index"
+# end
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  root :to => "users#sign_in"
 end
