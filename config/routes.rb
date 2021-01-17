8c493c0b39fc50869b6aa8a0367afaa68fa6bf14
Christopher Amanor <kriskit7@gmail.com>
2021-01-17 19:48:34 +0000

    Run database migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
-# Rails.application.routes.draw do
-  resources :messages
-  # resources :accounts
-#   devise_for :users
-#   root to: "users#index"
-# end
-
 Rails.application.routes.draw do
   resources :messages
   resources :accounts
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_16_203337) do
+ActiveRecord::Schema.define(version: 2021_01_17_192826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,18 @@ ActiveRecord::Schema.define(version: 2021_01_16_203337) do
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "sender"
+    t.string "destination"
+    t.string "source"
+    t.text "content"
+    t.string "status", default: "pending", null: false
+    t.bigint "account_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_messages_on_account_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,4 +52,5 @@ ActiveRecord::Schema.define(version: 2021_01_16_203337) do
   end
 
   add_foreign_key "accounts", "users"
+  add_foreign_key "messages", "accounts"
 end
